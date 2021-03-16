a1e96f8784471915d3fcfd33e57330066e80720d
Tina Haiser <tinahaiser@gmail.com>
2021-03-16 01:07:41 +0000

    finished all but the last translation
    Co-authored-by: Alena Davis <alenad@gmail.com>

--- trials.py
+++ trials.py
@@ -9,7 +9,7 @@ def output_all_items(items):
 def get_all_evens(nums):
     even_nums = []
     for num in nums:
-        if num % == 0:
+        if num % 2 == 0:
             even_nums.append(num)
 
     return even_nums
@@ -31,7 +31,7 @@ def print_as_numbered_list(items):
 
         i += 1
     
-return items
+    return items
 
 
 def get_range(start, stop):
@@ -46,29 +46,70 @@ def censor_vowels(word):
     chars = []
     vowels = "aeiou"
 
-  for letter in word: 
-    if letter in vowels:
-        chars.append('*')
-    else:
-        chars.append(letter)  
-  
-  return chars.join('')
+    for letter in word: 
+        if letter in vowels:
+            chars.append('*')
+        else:
+            chars.append(letter)  
+    
+    return chars.join('')
 
 
 def snake_to_camel(string):
-    pass  # TODO: replace this line with your code
+    words = string.split('_')
+    camel_case = []
+    first_word = words.pop(0)
+    camel_case.append(first_word.lower())
+    camelCase = ""
+
+    for word in words:
+        camel_case.append(word.title())
+
+    for word in camel_case:
+        camelCase += word    
+
+    return camelCase
+
 
 
 def longest_word_length(words):
-    pass  # TODO: replace this line with your code
+    longest = len(words[0])
+
+    for word in words:
+        if longest < len(word):
+            longest = len(word)
+    
+    return longest
+
 
 
 def truncate(string):
-    pass  # TODO: replace this line with your code
+    result = ""
+
+    for char in string:
+        if len(result) == 0 or char != result[-1]:
+            result += char
+        
+
+    return result
+
 
 
 def has_balanced_parens(string):
-    pass  # TODO: replace this line with your code
+    parens = 0
+
+    for char in string:
+        if char == '(':
+            parens += 1
+
+        elif char == ')':
+            parens -= 1
+
+    # if parens < 0:
+    #     return false
+
+    return parens == 0
+
 
 
 def compress(string):
